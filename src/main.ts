97ec2e588b58a3f778e03278e14c2ff80cfac930
xcy960815 <18763006837@163.com>
2025-01-16 20:38:01 +0800

    feat: 添加一些无用代码但不影响主体

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import App from './App.vue'
 import * as ElIcon from '@element-plus/icons-vue'
 import store from './store'
 
-// 你好你还哦啊
+// 你好你还哦啊12121212
 // @ts-ignore
 import locale from 'element-plus/dist/locale/zh-cn.mjs'
 import router from '@/router'
