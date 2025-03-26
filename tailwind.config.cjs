7521cecedcbb7141d9cfb46900ad4ef6771f9c3e
xcy960815 <18763006837@163.com>
2025-03-26 11:08:53 +0800

    feat: scss使用tailwind语法

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,15 +5,12 @@ const config = {
   content: ['./src/**/*.{js,jsx,ts,tsx,vue,scss}'], // 这里配置要使用Tailwind className的文件地址（tailwind亦将根据这些文件自动进行purge，移除未使用过的类名）
   theme: {
     extend: {
-      // 添加新的工具类
-      utilities: {
-        'whitespace-initial': {
-          'white-space': 'initial',
-        },
+      lineHeight:{
+        "24": "24px",
       },
-      colors:{
-        "bg-#f6f6f7": "#f6f6f7",
-      }
+      colors: {
+        "0d1117": "#0d1117",
+      },
     }
   },
   plugins: []
