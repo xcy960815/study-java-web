defc52d0049503caf1359783af0a04e8988c6ba1
xcy960815 <18763006837@163.com>
2025-03-26 11:23:33 +0800

    feat: ai输入框提供过快捷键操作

--- src/components/ai-chat/chat-input.vue
+++ src/components/ai-chat/chat-input.vue
@@ -2,30 +2,16 @@
   <!-- 问题输入框 & 发送按钮 -->
   <div class="flex items-center">
     <div class="flex-1 question-wrapper">
-      <el-input
-        v-model="question"
-        :autosize="{ minRows: 2 }"
-        class="w-full h-full text-sm rounded-md"
-        type="textarea"
-        placeholder="任意问题"
-      ></el-input>
+      <el-input v-model="question" :autosize="{ minRows: 4 }" class="w-full h-full text-sm rounded-md" type="textarea"
+        placeholder="任意问题"></el-input>
     </div>
     <div class="right-6 p-0.5 ml-5 flex items-center gap-2">
-      <el-button
-        @click="completions"
-        :disabled="buttonDisabled"
-        v-if="hasCompletionsButton(conversation)"
-        title="提交"
-        class="submit-question-button rounded-lg p-0.5"
-      >
+      <el-button @click="completions" :disabled="buttonDisabled" v-if="hasCompletionsButton(conversation)" title="提交"
+        class="submit-question-button rounded-lg p-0.5">
         <send theme="outline" :size="buttonnConfig.size" fill="#333" />
       </el-button>
-      <el-button
-        @click="cancelConversation"
-        v-if="hasCancelConversationButton(conversation)"
-        title="取消"
-        class="submit-question-button rounded-lg p-0.5"
-      >
+      <el-button @click="cancelConversation" v-if="hasCancelConversationButton(conversation)" title="取消"
+        class="submit-question-button rounded-lg p-0.5">
         <close-one :size="buttonnConfig.size" :fill="buttonnConfig.fill" />
       </el-button>
     </div>
@@ -34,8 +20,14 @@
 
 <script lang="ts" setup>
 import { Send, CloseOne } from '@icon-park/vue-next'
-import { ref, computed, type PropType } from 'vue'
+import { ref, computed, watch, type PropType } from 'vue'
 import { type Theme } from '@icon-park/vue-next/lib/runtime'
+import { UAParser } from 'ua-parser-js'
+import { useMagicKeys, useActiveElement } from '@vueuse/core'
+const parser = new UAParser()
+const magicKeys = useMagicKeys()
+const enterCommand = magicKeys['Meta+Enter']
+const enterCtrl = magicKeys['Ctrl+Enter']
 interface ButtonnConfig {
   size: number
   fill: string
@@ -44,7 +36,7 @@ interface ButtonnConfig {
 defineProps({
   conversation: {
     type: Object as PropType<AI.Gpt.AssistantConversation | null>,
-    default: () => {}
+    default: () => { }
   }
 })
 
@@ -88,5 +80,47 @@ const completions = () => {
 const cancelConversation = () => {
   emits('cancel-conversation')
 }
+
+/**
+ * 判断是否为 MacOS
+ */
+const isMacos = computed(() => {
+  const os = parser.getOS()
+  if (!os) return false
+
+  return os.name?.includes?.('macos')
+})
+console.log("isMacos-isMacos",isMacos);
+
+
+const activeElement = useActiveElement()
+
+const notUsingInput = computed(() => activeElement.value?.tagName !== 'INPUT')
+
+watch(
+  () => enterCommand.value,
+  () => {
+    if (!isMacos || notUsingInput.value) return
+    if (!enterCommand.value) {
+      emits('completions', question.value)
+    }
+  },
+  {
+    deep: true
+  }
+)
+
+watch(
+  () => enterCtrl.value,
+  () => {
+    if (isMacos || notUsingInput.value) return
+    if (!enterCtrl.value) {
+      emits('completions', question.value)
+    }
+  },
+  {
+    deep: true
+  }
+)
 </script>
 <style lang="less" scoped></style>
