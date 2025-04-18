76e1d4a8fe78ac961096596b6ad26004e5341257
xcy960815 <18763006837@163.com>
2025-04-18 16:34:44 +0800

    feat1: 测试格式化

--- .cz-config.cjs
+++ .cz-config.cjs
@@ -1,12 +1,11 @@
 module.exports = {
   types: [
-    { value: 'feat',     name: '✨ feat:     新功能' },
-    { value: 'fix',      name: '🐛 fix:      修复 Bug' },
-    { value: 'docs',     name: '📝 docs:     文档变更' },
-    { value: 'style',    name: '💄 style:    代码格式' },
-    { value: 'refactor',name: '♻️ refactor: 重构代码' },
-    { value: 'test',     name: '✅ test:     添加测试' },
-    { value: 'chore',    name: '🔧 chore:    构建/工具相关' },
+    { value: 'feat', name: '✨ feat:     新功能' },
+    { value: 'fix', name: '🐛 fix:      修复 Bug' },
+    { value: 'docs', name: '📝 docs:     文档变更' },
+    { value: 'style', name: '💄 style:    代码格式' },
+    { value: 'refactor', name: '♻️ refactor: 重构代码' },
+    { value: 'test', name: '✅ test:     添加测试' },
+    { value: 'chore', name: '🔧 chore:    构建/工具相关' },
   ],
-  // 其他配置项...
-};
+}
