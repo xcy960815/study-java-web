aa4520c9df484dfc84aafcd78db902302c00c754
xcy960815 <18763006837@163.com>
2025-07-15 17:34:39 +0800

    fix: 修复tailwind默认样式

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,22 @@
-/** 
- * @type {import('tailwindcss').Config} 
+/**
+ * @type {import('tailwindcss').Config}
  */
 const config = {
   content: ['./src/**/*.{js,jsx,ts,tsx,vue,scss}'], // 这里配置要使用Tailwind className的文件地址（tailwind亦将根据这些文件自动进行purge，移除未使用过的类名）
   theme: {
     extend: {
-      lineHeight:{
-        "24": "24px",
+      lineHeight: {
+        24: '24px',
       },
       colors: {
-        "0d1117": "#0d1117",
+        '0d1117': '#0d1117',
       },
-    }
+    },
   },
-  plugins: []
+  corePlugins: {
+    preflight: false, // 禁用 Tailwind 的基础样式重置，避免影响 Element Plus 组件样式
+  },
+  plugins: [],
 }
-
 
 module.exports = config
